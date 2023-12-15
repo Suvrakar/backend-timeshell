3584b994db27af0c37b7075b87c08c009f1f3a17
Debashis Kar Suvra <kar.suvra@gmail.com>
2023-12-15 23:19:09 +0600

    pg

--- config/database.js
+++ config/database.js
@@ -5,11 +5,11 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: env('DATABASE_HOST', 'localhost'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'mydatabase'),
-        username: env('DATABASE_USERNAME', 'mydatabaseuser'),
-        password: env('DATABASE_PASSWORD', 'mypassword'),
+        host: env('DATABASE_HOST', 'dpg-clthoq21hbls73efdgrg-a'), // Hostname
+        port: env.int('DATABASE_PORT', 5432), // Port
+        database: env('DATABASE_NAME', 'timeshell-db'), // Database name
+        username: env('DATABASE_USERNAME', 'suvra'), // Username
+        password: env('DATABASE_PASSWORD', 'luqoAYpLbLSPsUkUKCWJWz7q1xfSDi2E'), // Password
         ssl: env.bool('DATABASE_SSL', false),
       },
       options: {},
