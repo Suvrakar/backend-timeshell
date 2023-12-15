6f6c21eb5fce2fb81e0345e7f4b93bcd005832c0
Debashis Kar Suvra <kar.suvra@gmail.com>
2023-12-15 17:48:19 +0600

    db-added

--- config/database.js
+++ config/database.js
@@ -52,9 +52,9 @@ module.exports = ({ env }) => {
         connectionString: env('DATABASE_URL'),
         host: env('DATABASE_HOST', 'localhost'),
         port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
+        database: env('DATABASE_NAME', 'timeshell-db'),
+        user: env('DATABASE_USERNAME', 'suvra'),
+        password: env('DATABASE_PASSWORD', 'luqoAYpLbLSPsUkUKCWJWz7q1xfSDi2E'),
         ssl: env.bool('DATABASE_SSL', false) && {
           key: env('DATABASE_SSL_KEY', undefined),
           cert: env('DATABASE_SSL_CERT', undefined),
